59a2f36bc7c638f4fc52d55d241fd1e92af4960c
Ross Brodie <rcb547@users.noreply.github.com>
2022-06-06 12:34:14 +1000

    Improved configure-ubuntu-win10.sh script Updated visual studion to c++17

--- configure-ubuntu-win10.sh
+++ configure-ubuntu-win10.sh
@@ -3,7 +3,7 @@
 ### >> mpicc --showme
 ###    gcc -I/usr/lib/x86_64-linux-gnu/openmpi/include/openmpi -I/usr/lib/x86_64-linux-gnu/openmpi/include -pthread -L/usr/lib/x86_64-linux-gnu/openmpi/lib -lmpi 
 
-export LIBRJMCMC_ROOT=/mnt/c/Users/rossc/work/code/third_party/iearth/RJMCMC-1.0.11/ubuntu-win10/gnu
+export LIBRJMCMC_ROOT=$PWD/install-ubuntu-win10
 ./configure  --prefix=$LIBRJMCMC_ROOT --with-openmpi --with-openmpi-include-path=/usr/lib/x86_64-linux-gnu/openmpi/include --with-openmpi-lib-path=/usr/lib/x86_64-linux-gnu/openmpi/lib
 make clean
 make
